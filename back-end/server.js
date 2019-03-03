b11f7d8ad1063f50cdb49cc3a470476352632891
denchikUniversal <tkachenkodenys94@gmail.com>
2019-03-03 14:42:51 +0200

    feat(todo): mongoose connection, routes

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import Issue from './models/Issue';
+
 const app = express();
 const router = express.Router();
 
@@ -15,7 +17,26 @@ const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('MongoDB connection successfully')
-})
+});
+
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        } else res.json(issues);
+    });
+});
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err);
+        } else res.json(issue);
+    });
+});
+
+
+
 
 app.use('/', router);
 
