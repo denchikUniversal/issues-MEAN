167b3eaafc385fa90251c31199eb2aa45364930a
denchikUniversal <tkachenkodenys94@gmail.com>
2019-03-03 14:31:49 +0200

    feat(todo): adding server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
-app.get('/', (req, res) => res.send('Hello'));
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect();
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB connection successfully')
+})
+
+app.use('/', router);
+
+
 app.listen(4000, () => console.log('Express server running on port 4000'));
